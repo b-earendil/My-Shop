9b4fceef8380495afdce23dccb2176141150bdf5
Ben Adams <BKAdams@jhacorp.com>
2022-06-24 15:15:08 -0500

    Import CartService and Observable in shipping.component.ts and inject the cart service in the ShippingComponent constructor()

--- src/app/shipping/shipping.component.ts
+++ src/app/shipping/shipping.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-shipping',
@@ -7,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShippingComponent implements OnInit {
 
-  constructor() { }
+  constructor(private cartService: CartService) { }
 
   ngOnInit(): void {
   }
